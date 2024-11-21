36ce0bbef6823c4440637d8162072a787b604c89
Quentin Horgues <quentin.horgues@outlook.fr>
2024-11-21 17:46:15 +0100

    new question

--- src/quizData.js
+++ src/quizData.js
@@ -1,35 +1,49 @@
 const quizData = [
     {
       type: 'multiple-choice',
-      question: "Question 1",
-      options: ["Option 1", "Option 2", "Option 3", "Option 4"],
-      correctAnswers: ["Option 2", "Option 3"]
-    },
-    {
-      type: 'multiple-choice',
-      question: "Question 2",
-      options: ["Banane", "pomme"],
-      correctAnswers: ["Banane"]
+      question: "How long do we usually dream each night ?",
+      options: ["1 hours", "2 hours", "3 hours", "4 hours"],
+      correctAnswers: ["2 hours"]
     },
     {
       type: 'order',
       question: "Arrange sleep cycle stage in correct order",
       options: [
         { id: '1', content: "Vivid dreaming" },
-        { id: '2', content: "Deep sleep" },
-        { id: '3', content: "Light sleep" }
+        { id: '2', content: "The lightest stage of sleep"},
+        { id: '3', content: "Deep sleep" },
+        { id: '4', content: "Light sleep" }
       ],
       correctOrder: [
-        { id: '3', content: "Light sleep" },
-        { id: '2', content: "Deep sleep" },
+        { id: '2', content: "The lightest stage of sleep"},
+        { id: '4', content: "Light sleep" },
+        { id: '3', content: "Deep sleep" },
         { id: '1', content: "Vivid dreaming" }
       ]
     },
     {
       type: 'multiple-choice',
-      question: "Question 3",
-      options: ["Caramel", "Chocolat"],
-      correctAnswers: ["Chocolat"]
+      question: "What is the deep sleep time for one cycle ?",
+      options: ["20 - 40 min", "10 - 25 min", "15 - 30 min", "45 - 60 min"],
+      correctAnswers: ["20 - 40 min"]
+    },
+    {
+      type: 'multiple-choice',
+      question: "What is the recommanded sleep time ?",
+      options: ["6 hours", "7 hours", "7.30 hours", "8 hours", ],
+      correctAnswers: ["8 hours"]
+    },
+    {
+      type: 'multiple-choice',
+      question: "What are the many causes of nightmares ?",
+      options: ["Drug", "Fear", "Heartbreak", "Stress", "Illness", "Not eating enougt", "Alcoholism", "Traumatism"],
+      correctAnswers:  ["Drug", "Fear", "Stress", "Illness", "Traumatism"]
+    },
+    {
+      type: 'multiple-choice',
+      question: "What are the consequences of a bad night sleep ?",
+      options: ["Anxiety", "Smarter", "Stress", "Traumatism", "More focused", "Risk of cardiovascular problems"],
+      correctAnswers: ["Anxiety", "Stress", "Risk of cardiovascular problems"],
     }
     // Ajoute plus de questions ici
   ];
